4a75023e7e56668c4f00133f23b6f567a4d0a909
Ashish Anand <ashishthedev@gmail.com>
2014-12-12 18:52:14 +0530

    Items are being added and deleted

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,15 +4,28 @@ appMod.controller('ngRRKDailyProdController', ['$scope', '$http', function($scop
   var api = "/api/rrkDailyProdEmailSendApi";
   var postData = null;
 
-  $scope.statusNote = "Fetching...";
-  $http.post(api, postData).success(function(data, status, headers, config) {
-    $scope.statusNote = "";
-  }).error(function(data, status, headers, config){
-    $scope.statusNote = "There was an error. Thats all there is to it. Please try again after some time";
-  });
+  $scope.removeEntry = function(index) {
+    $scope.items.splice(index, 1);
+  }
 
+  $scope.addSingleEntry = function() {
+    l = $scope.items.length;
+    $scope.items.splice(l, 0, angular.copy($scope.entry));
+  }
+
+  $scope.submitTodaysLog = function() {
+    $scope.statusNote = "Sending...";
+    $http.post(api, postData).success(function(data, status, headers, config) {
+      $scope.statusNote = "";
+    }).error(function(data, status, headers, config){
+      $scope.statusNote = "There was an error. Thats all there is to it. Please try again after some time";
+    });
+  }
 
   $scope.dateValue =  new Date();
+  $scope.entry = {modelName:"a", quantity:5, unit:"pc", remarks:"none"};
+  $scope.items = [];
+  $scope.statusNote = "";
 
 }]);
 
