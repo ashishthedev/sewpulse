6c678c8fe109c94ad4f188c687c811e4b45d63cd
Ashish Anand <ashishthedev@gmail.com>
2015-01-06 14:08:33 +0530

    Daily assembly and production are separetly implemented

--- rrk.go
+++ rrk.go
@@ -17,6 +17,10 @@ func initRRKUrlMaps() {
 			handler:      rrkGeneralPageHander,
 			templatePath: "templates/rrk_daily_production.html",
 		},
+		"/rrk/daily-assembly": urlStruct{
+			handler:      rrkGeneralPageHander,
+			templatePath: "templates/rrk_daily_assembly.html",
+		},
 		"/rrk": urlStruct{
 			handler:      rrkGeneralPageHander,
 			templatePath: "templates/rrk.html",
@@ -34,6 +38,9 @@ func initRRKApiMaps() {
 	apiMaps = map[string]apiStruct{
 		"/api/rrkDailyProdEmailSendApi": apiStruct{
 			handler: rrkDailyProdEmailSendApiHandler,
+		},
+		"/api/rrkDailyAssemblyEmailSendApi": apiStruct{
+			handler: rrkDailyAssemblyEmailSendApiHandler,
 		},
 	}
 
@@ -60,6 +67,98 @@ type ProducedItem struct {
 type ProducedItemsJSONValues struct {
 	DateTimeAsUTCMilliSeconds int64
 	Items                     []ProducedItem
+}
+
+func rrkDailyAssemblyEmailSendApiHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var producedItemsAsJson ProducedItemsJSONValues
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&producedItemsAsJson); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	submissionDateTimeAsUTC := producedItemsAsJson.DateTimeAsUTCMilliSeconds
+	logTime := time.Unix(submissionDateTimeAsUTC/1000, 0)
+	logDateDDMMYY := DDMMYYFromUTC(submissionDateTimeAsUTC)
+	logMsg := LogMsgShownForLogTime(logTime, time.Now())
+
+	totalQuantityProduced := 0
+	for _, pi := range producedItemsAsJson.Items {
+		totalQuantityProduced += pi.Quantity
+	}
+
+	htmlTable := fmt.Sprintf(`
+	<table border=1 cellpadding=5>
+	<caption>
+	<u><h1>%s</h1></u>
+	<u><h3>%s</h3></u>
+	</caption>
+	<thead>
+	<tr bgcolor=#838468>
+	<th><font color='#000000'> Product </font></th>
+	<th><font color='#000000'> Line </font></th>
+	<th><font color='#000000'> Quantity </font></th>
+	<th><font color='#000000'> Units </font></th>
+	<th><font color='#000000'> Remarks </font></th>
+	</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<td colspan=2>Total:</td>
+			<td colspan=3><font color="#DD472F"><b>%v</b></font></td>
+		</tr>
+	</tfoot>
+	`,
+		logDateDDMMYY,
+		logMsg,
+		totalQuantityProduced,
+	)
+
+	for _, pi := range producedItemsAsJson.Items {
+		htmlTable +=
+			fmt.Sprintf(`
+		<tr>
+		<td>%s</td>
+		<td>%s</td>
+		<td>%d</td>
+		<td>%s</td>
+		<td>%s</td>
+		</tr>`, pi.ModelName, pi.AssemblyLineName, pi.Quantity, pi.Unit, pi.Remarks)
+	}
+	htmlTable += "</table>"
+
+	finalHTML := fmt.Sprintf("<html><head></head><body>%s</body></html>", htmlTable)
+
+	bccAddr := Reverse("moc.liamg@dnanatodhsihsa")
+	toAddr := ""
+	if IsLocalHostedOrOnDevBranch(r) {
+		toAddr = Reverse("moc.liamg@dnanatodhsihsa")
+	} else {
+		toAddr = Reverse("moc.liamg@ztigihba")
+	}
+
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	msg := &mail.Message{
+		Sender:   u.String() + "<" + u.Email + ">",
+		To:       []string{toAddr},
+		Bcc:      []string{bccAddr},
+		Subject:  fmt.Sprintf("%s: %v pc [SEWPULSE][RRKDA]", logDateDDMMYY, totalQuantityProduced),
+		HTMLBody: finalHTML,
+	}
+
+	if err := mail.Send(c, msg); err != nil {
+		c.Errorf("Couldn't send email: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	return
 }
 
 func rrkDailyProdEmailSendApiHandler(w http.ResponseWriter, r *http.Request) {
---
+++ static/js/rrkDailyAssemblyController.js
@@ -0,0 +1,56 @@
+﻿var appMod = angular.module('ngSEWPulseApp', []);
+
+appMod.controller('ngRRKDailyAssemblyController', ['$scope', '$http', function($scope, $http) {
+
+  function UpdateTotalQty() {
+    var t = 0;
+    for (var i=0; i < $scope.items.length; i++) {
+      t += parseInt($scope.items[i].quantity);
+      console.log(t);
+    }
+    $scope.totalQuantityProduced = t;
+  }
+
+  $scope.removeEntry = function(index) {
+    $scope.items.splice(index, 1);
+    UpdateTotalQty();
+  }
+
+  $scope.addSingleEntry = function() {
+    var l = $scope.items.length;
+    $scope.items.splice(l, 0, angular.copy($scope.entry));
+    UpdateTotalQty();
+  }
+
+  $scope.DateChanged = function() {
+    var today = new Date();
+    if (today < $scope.dateValue) {
+      $scope.dateValue = today;
+    }
+  }
+
+  $scope.submitTodaysLog = function() {
+    $scope.statusNote = "Submitting...";
+    var api = "/api/rrkDailyAssemblyEmailSendApi";
+    var postData = {
+      "dateTimeAsUTCMilliSeconds": $scope.dateValue.getTime(),
+      "items": $scope.items,
+    }
+
+    $http.post(api, postData).success(function(data, status, headers, config) {
+      $scope.statusNote = "";
+      $scope.isLogSubmitted = true;
+    }).error(function(data, status, headers, config){
+      $scope.statusNote = status + ": " + data;
+      $scope.isLogSubmitted = false;
+    });
+  }
+
+  $scope.dateValue =  new Date();
+  $scope.entry = {modelName:"Premium Plus", quantity:5, assemblyLineName: "Line1", unit:"pc"};
+  $scope.items = [];
+  $scope.statusNote = "";
+  $scope.isLogSubmitted = false;
+  $scope.totalQuantityProduced = 0;
+
+}]);
